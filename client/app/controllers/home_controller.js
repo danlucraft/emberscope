82ff9522b58fda66ce8a30f4d47569f248a6bfe2
Daniel Lucraft <dan@fluentradical.com>
2014-04-05 16:21:39 +0100

    Sort posts by vote on the home page

--- client/app/controllers/home_controller.js
+++ client/app/controllers/home_controller.js
@@ -1,6 +1,7 @@
 
 App.HomeController = Ember.ArrayController.extend({
-  needs: "application",
-  loggedInUser: Ember.computed.alias("controllers.application.loggedInUser"),
-
+  needs:          "application",
+  loggedInUser:   Ember.computed.alias("controllers.application.loggedInUser"),
+  sortProperties: ["voteCount"],
+  sortAscending:  false,
 });
