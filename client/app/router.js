206dd0c8369b97dd9d8836407426ae4328d3cc4c
Daniel Lucraft <dan@fluentradical.com>
2014-04-27 09:47:30 +0100

    Add rudimentary user page

--- client/app/router.js
+++ client/app/router.js
@@ -3,6 +3,8 @@ App.Router.map(function() {
   this.route("home", { path: "/" });
   this.resource("post", { path: "/posts/:post_id" }, function() {
   });
+  this.resource("user", { path: "/users/:user_id" }, function() {
+  });
 });
 
 App.MissingRoute = Em.Route.extend({
